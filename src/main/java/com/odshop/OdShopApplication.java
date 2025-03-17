64b2cd4c392d112af6782d7e3bdfeb69322edce0
od <od@sweettracker.co.kr>
2025-03-17 22:43:36 +0900

    feat: project init

---
+++ src/main/java/com/odshop/OdShopApplication.java
@@ -0,0 +1,13 @@
+package com.odshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odshop/OdShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
