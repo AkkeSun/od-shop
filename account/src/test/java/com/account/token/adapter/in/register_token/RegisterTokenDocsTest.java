2938dc554e25bc54e2819fa445de87d079b22ce9
od <od@sweettracker.co.kr>
2025-03-24 22:17:25 +0900

    test: account (docs test 추가)

---
+++ account/src/test/java/com/account/token/adapter/in/register_token/RegisterTokenDocsTest.java
@@ -0,0 +1,189 @@
+package com.account.token.adapter.in.register_token;
+
+import static com.account.global.exception.ErrorCode.DoesNotExist_ACCOUNT_INFO;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.account.RestDocsSupport;
+import com.account.global.exception.CustomNotFoundException;
+import com.account.token.application.port.in.RegisterTokenUseCase;
+import com.account.token.application.service.register_token.RegisterTokenServiceResponse;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+class RegisterTokenDocsTest extends RestDocsSupport {
+
+    private final RegisterTokenUseCase registerTokenUseCase =
+        mock(RegisterTokenUseCase.class);
+
+    @Override
+    protected Object initController() {
+        return new RegisterTokenController(registerTokenUseCase);
+    }
+
+    private void performPatchAndDocument(RegisterTokenRequest request,
+        ResultMatcher status, String docIdentifier, String responseSchema,
+        FieldDescriptor... responseFields) throws Exception {
+
+        mockMvc.perform(post("/auth/login")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status)
+            .andDo(MockMvcRestDocumentationWrapper.document(docIdentifier,
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    resource(ResourceSnippetParameters.builder()
+                        .tag("Token")
+                        .summary("인증 토큰 발급 API")
+                        .description("이메일과 비밀번호로 인증 토큰을 발급하는 API 입니다.")
+                        .requestFields(
+                            fieldWithPath("email").type(JsonFieldType.STRING)
+                                .description("이메일"),
+                            fieldWithPath("password").type(JsonFieldType.STRING)
+                                .description("비밀번호")
+                        )
+                        .responseFields(responseFields)
+                        .requestSchema(Schema.schema("[REQUEST] register-token"))
+                        .responseSchema(Schema.schema(responseSchema))
+                        .build()
+                    )
+                )
+            );
+    }
+
+    @Nested
+    @DisplayName("[registerToken] 사용자 토큰을 등록하는 API")
+    class Describe_RegisterToken {
+
+        @Test
+        @DisplayName("[success] 필수값을 올바르게 입력했을 때 200 코드와 성공 메시지를 응답한다.")
+        void success() throws Exception {
+            // given
+            RegisterTokenRequest request = RegisterTokenRequest.builder()
+                .email("test@gmail.com")
+                .password("1234")
+                .build();
+            RegisterTokenServiceResponse response = RegisterTokenServiceResponse.builder()
+                .accessToken("Bearer testAccessToken")
+                .refreshToken("Bearer testRefreshToken")
+                .build();
+            given(registerTokenUseCase.registerToken(request.toCommand()))
+                .willReturn(response);
+
+            // when // then
+            performPatchAndDocument(request, status().isOk(),
+                "[registerToken] SUCCESS",
+                "[RESPONSE] register-token",
+                fieldWithPath("httpStatus").type(JsonFieldType.NUMBER)
+                    .description("상태 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING)
+                    .description("상태 메시지"),
+                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                    .description("응답 데이터"),
+                fieldWithPath("data.accessToken").type(JsonFieldType.STRING)
+                    .description("액세스 토큰"),
+                fieldWithPath("data.refreshToken").type(JsonFieldType.STRING)
+                    .description("리프레시 토큰")
+            );
+        }
+
+
+        @Test
+        @DisplayName("[error] 이메일을 입력하지 않았을 때 400 코드와 오류 메시지를 응답한다.")
+        void error1() throws Exception {
+            // given
+            RegisterTokenRequest request = RegisterTokenRequest.builder()
+                .email("")
+                .password("1234")
+                .build();
+
+            // when // then
+            performPatchAndDocument(request, status().is4xxClientError(),
+                "[registerToken] 이메일 미입력",
+                "[RESPONSE] ERROR",
+                fieldWithPath("httpStatus").type(JsonFieldType.NUMBER)
+                    .description("상태 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING)
+                    .description("상태 메시지"),
+                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                    .description("응답 데이터"),
+                fieldWithPath("data.errorCode").type(JsonFieldType.NUMBER)
+                    .description("에러 코드"),
+                fieldWithPath("data.errorMessage").type(JsonFieldType.STRING)
+                    .description("에러 메시지")
+            );
+        }
+
+        @Test
+        @DisplayName("[error] 비밀번호를 입력하지 않았을 때 400 코드와 오류 메시지를 응답한다.")
+        void error2() throws Exception {
+            // given
+            RegisterTokenRequest request = RegisterTokenRequest.builder()
+                .email("test@gmail.com")
+                .password("")
+                .build();
+
+            // when // then
+            performPatchAndDocument(request, status().is4xxClientError(),
+                "[registerToken] 비밀번호 미입력",
+                "[RESPONSE] ERROR",
+                fieldWithPath("httpStatus").type(JsonFieldType.NUMBER)
+                    .description("상태 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING)
+                    .description("상태 메시지"),
+                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                    .description("응답 데이터"),
+                fieldWithPath("data.errorCode").type(JsonFieldType.NUMBER)
+                    .description("에러 코드"),
+                fieldWithPath("data.errorMessage").type(JsonFieldType.STRING)
+                    .description("에러 메시지")
+            );
+        }
+
+        @Test
+        @DisplayName("[error] 조회된 사용자 정보가 없을 때 404 코드와 오류 메시지를 응답한다.")
+        void error3() throws Exception {
+            // given
+            RegisterTokenRequest request = RegisterTokenRequest.builder()
+                .email("test@gmail.com")
+                .password("1111111")
+                .build();
+            given(registerTokenUseCase.registerToken(request.toCommand()))
+                .willThrow(new CustomNotFoundException(DoesNotExist_ACCOUNT_INFO));
+
+            // when // then
+            performPatchAndDocument(request, status().is4xxClientError(),
+                "[registerToken] 조회된 사용자 정보 없음",
+                "[RESPONSE] ERROR",
+                fieldWithPath("httpStatus").type(JsonFieldType.NUMBER)
+                    .description("상태 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING)
+                    .description("상태 메시지"),
+                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                    .description("응답 데이터"),
+                fieldWithPath("data.errorCode").type(JsonFieldType.NUMBER)
+                    .description("에러 코드"),
+                fieldWithPath("data.errorMessage").type(JsonFieldType.STRING)
+                    .description("에러 메시지")
+            );
+        }
+    }
+
+}
