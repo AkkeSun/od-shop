82880c96c12e91bf376bb854e1c1b0ca66764c0f
od <akkessun@gmail.com>
2025-03-26 13:05:02 +0900

    feat: account-agent (모듈 초기화)

---
+++ account-agent/src/main/java/com/accountagent/AccountAgentApplication.java
@@ -0,0 +1,13 @@
+package com.accountagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountAgentApplication.class, args);
+    }
+
+}
---
+++ account-agent/src/test/java/com/accountagent/AccountAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
