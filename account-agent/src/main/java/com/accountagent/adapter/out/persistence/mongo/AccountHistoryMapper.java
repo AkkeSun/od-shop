299d3cc5c1d238ac7229b77d89632987429e3986
od <Akkessun@gmail.com>
2025-03-27 01:36:06 +0900

    feat: account-agent (mapper static 처리)

--- account-agent/src/main/java/com/accountagent/adapter/out/persistence/mongo/AccountHistoryMapper.java
+++ account-agent/src/main/java/com/accountagent/adapter/out/persistence/mongo/AccountHistoryMapper.java
@@ -1,12 +1,10 @@
 package com.accountagent.adapter.out.persistence.mongo;
 
 import com.accountagent.domain.model.AccountHistory;
-import org.springframework.stereotype.Component;
 
-@Component
 class AccountHistoryMapper {
 
-    public AccountHistoryDocument toDocument(AccountHistory domain) {
+    public static AccountHistoryDocument toDocument(AccountHistory domain) {
         return AccountHistoryDocument.builder()
             .accountId(domain.accountId())
             .type(domain.type())
--- account-agent/src/main/java/com/accountagent/adapter/out/persistence/mongo/DlqLogMapper.java
+++ account-agent/src/main/java/com/accountagent/adapter/out/persistence/mongo/DlqLogMapper.java
@@ -1,12 +1,10 @@
 package com.accountagent.adapter.out.persistence.mongo;
 
 import com.accountagent.domain.model.DlqLog;
-import org.springframework.stereotype.Component;
 
-@Component
 class DlqLogMapper {
 
-    public DlqLogDocument toDocument(DlqLog domain) {
+    public static DlqLogDocument toDocument(DlqLog domain) {
         return DlqLogDocument.builder()
             .topic(domain.topic())
             .payload(domain.payload())
