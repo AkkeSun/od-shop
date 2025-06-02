16a2e347f502e135e39cfae7f2455196f14f7c1b
od <Akkessun@gmail.com>
2025-06-02 17:02:32 +0900

    feat: account (cors 설정 추가)

--- account/src/main/java/com/account/infrastructure/config/WebConfig.java
+++ account/src/main/java/com/account/infrastructure/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
         this.loginAccountResolver = loginAccountResolver;
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*");
+    }
+    
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginAccountResolver);
