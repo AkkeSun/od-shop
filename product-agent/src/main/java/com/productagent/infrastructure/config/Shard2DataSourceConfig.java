1737ca961f69eb6a53510f559dabd775b652acce
od <Akkessun@gmail.com>
2025-07-13 23:26:39 +0900

    feat(product-agent): dbConfig 추가

---
+++ product-agent/src/main/java/com/productagent/infrastructure/config/Shard2DataSourceConfig.java
@@ -0,0 +1,71 @@
+package com.productagent.infrastructure.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "com.productagent.adapter.out.persistence.jpa.shard2",
+    entityManagerFactoryRef = "secondaryEntityManagerFactory",
+    transactionManagerRef = "secondaryTransactionManager"
+)
+public class Shard2DataSourceConfig {
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.shard2")
+    public DataSourceProperties secondaryDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource secondaryDataSource(
+        @Qualifier("secondaryDataSourceProperties") DataSourceProperties dataSourceProperties
+    ) {
+        return dataSourceProperties
+            .initializeDataSourceBuilder()
+            .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(
+        EntityManagerFactoryBuilder builder,
+        @Qualifier("secondaryDataSource") DataSource dataSource
+    ) {
+        Map<String, Object> props = new HashMap<>();
+        props.put("hibernate.hbm2ddl.auto", ddlAuto);
+        props.put("hibernate.physical_naming_strategy",
+            "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
+
+        return builder
+            .dataSource(dataSource)
+            .packages("com.product.adapter.out.persistence.jpa.shard2")
+            .persistenceUnit("secondaryEntityManager")
+            .properties(props)
+            .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager secondaryTransactionManager(
+        @Qualifier("secondaryEntityManagerFactory") EntityManagerFactory entityManagerFactory
+    ) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
