b9bfdd393546b5eecea6584d42e3c7e618d242a9
od <Akkessun@gmail.com>
2025-07-15 13:48:32 +0900

    feat(product): 클릭 로그 도메인 수정

--- product/src/main/java/com/product/domain/model/ProductClickLog.java
+++ product/src/main/java/com/product/domain/model/ProductClickLog.java
@@ -1,6 +1,5 @@
 package com.product.domain.model;
 
-import static com.product.infrastructure.util.DateUtil.getCurrentDate;
 import static com.product.infrastructure.util.DateUtil.getCurrentDateTime;
 
 import lombok.Builder;
@@ -8,14 +7,12 @@ import lombok.Builder;
 @Builder
 public record ProductClickLog(
     Long productId,
-    String regDate,
     String regDateTime
 ) {
 
     public static ProductClickLog of(Long productId) {
         return ProductClickLog.builder()
             .productId(productId)
-            .regDate(getCurrentDate())
             .regDateTime(getCurrentDateTime())
             .build();
     }
