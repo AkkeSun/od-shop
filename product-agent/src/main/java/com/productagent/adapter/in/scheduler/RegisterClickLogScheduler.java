83cbc3624821d489336d8e6f394ad43f055106e1
od <Akkessun@gmail.com>
2025-07-15 14:10:54 +0900

    refactor(product-agent): 토픽명 상수 처리

--- product-agent/src/main/java/com/productagent/adapter/in/scheduler/RegisterClickLogScheduler.java
+++ product-agent/src/main/java/com/productagent/adapter/in/scheduler/RegisterClickLogScheduler.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,22 +15,25 @@ import org.springframework.stereotype.Component;
 @Component
 class RegisterClickLogScheduler {
 
+    private final String topicName;
     private final KafkaBatchHandler batchHandler;
     private final Consumer<String, String> consumer;
     private final RegisterClickLogUseCase registerClickLogUseCase;
 
     RegisterClickLogScheduler(ConsumerFactory<String, String> registerClickConsumerFactory,
         KafkaBatchHandler batchHandler,
-        RegisterClickLogUseCase registerClickLogUseCase) {
+        RegisterClickLogUseCase registerClickLogUseCase,
+        @Value("${kafka.topic.click}") String topicName) {
+        this.topicName = topicName;
         this.batchHandler = batchHandler;
         this.registerClickLogUseCase = registerClickLogUseCase;
         this.consumer = registerClickConsumerFactory.createConsumer();
-        consumer.subscribe(Collections.singletonList("product-click"));
+        consumer.subscribe(Collections.singletonList(topicName));
     }
 
     @Scheduled(cron = "0 0/1 * * * *")
     @SchedulerLock(name = "product-click", lockAtLeastFor = "1s", lockAtMostFor = "30s")
-    void registerHistory() {
-        batchHandler.handle(consumer, "product-click", registerClickLogUseCase::register);
+    void registerClickLog() {
+        batchHandler.handle(consumer, topicName, registerClickLogUseCase::register);
     }
 }
--- product-agent/src/main/java/com/productagent/adapter/in/scheduler/RegisterHistoryScheduler.java
+++ product-agent/src/main/java/com/productagent/adapter/in/scheduler/RegisterHistoryScheduler.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,22 +15,25 @@ import org.springframework.stereotype.Component;
 @Component
 class RegisterHistoryScheduler {
 
+    private final String topicName;
     private final KafkaBatchHandler batchHandler;
     private final Consumer<String, String> consumer;
     private final RegisterHistoryUseCase registerHistoryUseCase;
 
     RegisterHistoryScheduler(ConsumerFactory<String, String> registerHistoryConsumerFactory,
         KafkaBatchHandler batchHandler,
-        RegisterHistoryUseCase registerHistoryUseCase) {
+        RegisterHistoryUseCase registerHistoryUseCase,
+        @Value("${kafka.topic.history}") String topicName) {
+        this.topicName = topicName;
         this.batchHandler = batchHandler;
         this.registerHistoryUseCase = registerHistoryUseCase;
         this.consumer = registerHistoryConsumerFactory.createConsumer();
-        consumer.subscribe(Collections.singletonList("product-history"));
+        consumer.subscribe(Collections.singletonList(topicName));
     }
 
     @Scheduled(cron = "0 0/1 * * * *")
     @SchedulerLock(name = "product-history", lockAtLeastFor = "1s", lockAtMostFor = "30s")
     void registerHistory() {
-        batchHandler.handle(consumer, "product-history", registerHistoryUseCase::register);
+        batchHandler.handle(consumer, topicName, registerHistoryUseCase::register);
     }
 }
