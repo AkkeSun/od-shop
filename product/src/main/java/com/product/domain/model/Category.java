accb1125e437a10a43d71245a4c764492a0408c7
od <akkessun@gmail.com>
2025-07-22 16:38:48 +0900

    feat(product): 카테고리 수정

--- product/src/main/java/com/product/domain/model/Category.java
+++ product/src/main/java/com/product/domain/model/Category.java
@@ -4,17 +4,11 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum Category {
-    ELECTRONICS("전자제품"),
+    DIGITAL("전자제품"),
     FASHION("패션"),
-    HOME_APPLIANCES("가전제품"),
-    BEAUTY("뷰티"),
-    BOOKS("도서"),
     SPORTS("스포츠"),
     FOOD("식품"),
-    TOYS("장난감"),
-    FURNITURE("가구"),
-    AUTOMOTIVE("자동차 용품"),
-    HEALTH("건강"),
+    LIFE("생활용품"),
     TOTAL("전체");
 
     private final String description;
