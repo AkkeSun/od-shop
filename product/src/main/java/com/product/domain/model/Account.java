773b0e316329cb9c298edfb57f88471e504a902d
od <Akkessun@gmail.com>
2025-07-31 11:28:48 +0900

    feat: product (토큰 다중 권한 처리되도록 수정)

--- product/src/main/java/com/product/domain/model/Account.java
+++ product/src/main/java/com/product/domain/model/Account.java
@@ -1,20 +1,22 @@
 package com.product.domain.model;
 
 import io.jsonwebtoken.Claims;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record Account(
     Long id,
     String email,
-    String role
+    List<String> roles
 ) {
 
     public static Account of(Claims claims) {
         return Account.builder()
             .email(claims.getSubject())
             .id(Long.valueOf(claims.get("accountId").toString()))
-            .role(claims.get("role").toString())
+            .roles(Arrays.stream(claims.get("roles").toString().split(",")).toList())
             .build();
     }
 }
