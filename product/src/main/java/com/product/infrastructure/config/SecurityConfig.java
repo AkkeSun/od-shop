5780517a19ea75c19b68e7d11898c176df77b015
od <Akkessun@gmail.com>
2025-07-31 11:30:43 +0900

    feat: product (DB 기반 인가 처리하도록 개선: SecurityConfig 수정)

--- product/src/main/java/com/product/infrastructure/config/SecurityConfig.java
+++ product/src/main/java/com/product/infrastructure/config/SecurityConfig.java
@@ -1,26 +1,20 @@
 package com.product.infrastructure.config;
 
-
-import static com.product.infrastructure.util.JsonUtil.toJsonString;
-
-import com.product.infrastructure.exception.ErrorCode;
-import com.product.infrastructure.exception.ErrorResponse;
+import com.product.infrastructure.filter.AuthorizationFilter;
 import com.product.infrastructure.filter.CustomErrorLogFilter;
 import com.product.infrastructure.filter.JwtAuthenticationFilter;
-import com.product.infrastructure.response.ApiResponse;
-import java.nio.charset.StandardCharsets;
+import com.product.infrastructure.handler.CustomAccessDeniedHandler;
+import com.product.infrastructure.handler.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -31,7 +25,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtFilter;
-    private final CustomErrorLogFilter requestBodyCachingFilter;
+    private final CustomErrorLogFilter errorLogFilter;
+    private final AuthorizationFilter authorizationFilter;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -42,53 +39,17 @@ public class SecurityConfig {
             .sessionManagement(session -> {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
             })
-            .addFilterBefore(requestBodyCachingFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(errorLogFilter, UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
             // --------------- 인가 정책 ---------------
-            .authorizeHttpRequests(auth -> {
-                auth.requestMatchers(HttpMethod.GET, "/products/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/products").hasRole("SELLER")
-                    .requestMatchers(HttpMethod.PUT, "/products/{productId}").hasRole("SELLER")
-                    .requestMatchers(HttpMethod.DELETE, "/products/{productId}").hasRole("SELLER")
-                    .requestMatchers(HttpMethod.POST, "/products/{productId}/reviews")
-                    .hasRole("CUSTOMER")
-                    .anyRequest().authenticated();
-            })
+            .addFilterAfter(authorizationFilter, ExceptionTranslationFilter.class)
 
             // --------------- 인증/인가 예외처리 ---------------
-            .exceptionHandling(exception -> {
-                exception.authenticationEntryPoint((req, res, e) -> {
-                    /*
-				        예외 발생시 스프링 컨테이너에서 바디 값을 읽어와
-				        ContentCachingRequestWrapper 에 요청 바디 정보를 캐시할 수 있도록 합니다.
-				     */
-                    StreamUtils.copyToString(req.getInputStream(), StandardCharsets.UTF_8);
-                    String responseBody = toJsonString(ApiResponse.of(
-                        HttpStatus.UNAUTHORIZED,
-                        ErrorResponse.builder()
-                            .errorCode(ErrorCode.INVALID_ACCESS_TOKEN_BY_SECURITY.getCode())
-                            .errorMessage(ErrorCode.INVALID_ACCESS_TOKEN_BY_SECURITY.getMessage())
-                            .build()));
-                    res.setContentType("application/json;charset=UTF-8");
-                    res.setStatus(HttpStatus.UNAUTHORIZED.value());
-                    res.getWriter().write(responseBody);
-                });
-
-                exception.accessDeniedHandler((req, res, e) -> {
-
-                    StreamUtils.copyToString(req.getInputStream(), StandardCharsets.UTF_8);
-                    String responseBody = toJsonString(ApiResponse.of(
-                        HttpStatus.FORBIDDEN,
-                        ErrorResponse.builder()
-                            .errorCode(ErrorCode.ACCESS_DENIED_BY_SECURITY.getCode())
-                            .errorMessage(ErrorCode.ACCESS_DENIED_BY_SECURITY.getMessage())
-                            .build()));
-                    res.setContentType("application/json;charset=UTF-8");
-                    res.setStatus(HttpStatus.FORBIDDEN.value());
-                    res.getWriter().write(responseBody);
-                });
-            })
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+            )
 
             //--------------- csrf, cors 설정 ---------------
             .csrf(AbstractHttpConfigurer::disable)
