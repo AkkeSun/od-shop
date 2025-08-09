8df147af65954e9f98fe802bbf4e65defb20effa
od <akkessun@gmail.com>
2025-08-09 19:31:34 +0900

    feat(product): 상품 예약 확정 RPC 개발 (RPC 개발)

---
+++ product/src/main/java/com/product/adapter/in/grpc/ConfirmReservationStub.java
@@ -0,0 +1,53 @@
+package com.product.adapter.in.grpc;
+
+import static com.product.infrastructure.util.JsonUtil.toJsonString;
+
+import com.product.application.port.in.ConfirmReservationUseCase;
+import com.product.application.port.in.command.ConfirmReservationCommand;
+import com.product.application.service.confirm_reeservation.ConfirmReservationServiceResponse;
+import com.product.infrastructure.util.GrpcUtil;
+import grpc.product.ConfirmProductReservationRequest;
+import grpc.product.ConfirmProductReservationResponse;
+import grpc.product.ConfirmProductReservationServiceGrpc.ConfirmProductReservationServiceImplBase;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+@RequiredArgsConstructor
+public class ConfirmReservationStub extends ConfirmProductReservationServiceImplBase {
+
+    private final ConfirmReservationUseCase useCase;
+
+    @Override
+    public void confirmReservation(ConfirmProductReservationRequest request,
+        StreamObserver<ConfirmProductReservationResponse> responseObserver) {
+        try {
+
+            log.info("[gRPC] confirmReservation request - {}", toJsonString(request));
+
+            ConfirmReservationServiceResponse response = useCase.confirmReservation(
+                ConfirmReservationCommand.builder()
+                    .productId(request.getProductId())
+                    .reserveId(request.getReservationId())
+                    .build());
+
+            responseObserver.onNext(ConfirmProductReservationResponse.newBuilder()
+                .setResult(response.result())
+                .build());
+            responseObserver.onCompleted();
+            log.info("[gRPC] confirmReservation response - {}", toJsonString(response));
+
+        } catch (Exception e) {
+            Status status = GrpcUtil.getStatus(e);
+            log.info("[gRPC] confirmReservation error - {}", toJsonString(status));
+
+            responseObserver.onError(status
+                .withDescription(status.getDescription())
+                .asRuntimeException());
+        }
+    }
+}
