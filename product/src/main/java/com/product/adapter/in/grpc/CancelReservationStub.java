08b0184631bb8e7a10cc7cd3542ee68397696167
od <akkessun@gmail.com>
2025-08-09 19:54:22 +0900

    feat(product): 예약 상품 취소 RPC 개발 (RPC 개발)

---
+++ product/src/main/java/com/product/adapter/in/grpc/CancelReservationStub.java
@@ -0,0 +1,52 @@
+package com.product.adapter.in.grpc;
+
+import static com.product.infrastructure.util.JsonUtil.toJsonString;
+
+import com.product.application.port.in.CancelReservationUseCase;
+import com.product.application.port.in.command.CancelReservationCommand;
+import com.product.application.service.cancel_reservation.CancelReservationServiceResponse;
+import com.product.infrastructure.util.GrpcUtil;
+import grpc.product.CancelProductReservationRequest;
+import grpc.product.CancelProductReservationResponse;
+import grpc.product.CancelProductReservationServiceGrpc.CancelProductReservationServiceImplBase;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+@RequiredArgsConstructor
+public class CancelReservationStub extends CancelProductReservationServiceImplBase {
+
+    private final CancelReservationUseCase useCase;
+
+    @Override
+    public void cancelReservation(CancelProductReservationRequest request,
+        StreamObserver<CancelProductReservationResponse> responseObserver) {
+        try {
+
+            log.info("[gRPC] cancelReservation request - {}", toJsonString(request));
+
+            CancelReservationServiceResponse response = useCase.cancel(CancelReservationCommand.builder()
+                    .productId(request.getProductId())
+                    .reserveId(request.getReservationId())
+                    .build());
+
+            responseObserver.onNext(CancelProductReservationResponse.newBuilder()
+                .setResult(response.result())
+                .build());
+            responseObserver.onCompleted();
+            log.info("[gRPC] cancelReservation response - {}", toJsonString(response));
+
+        } catch (Exception e) {
+            Status status = GrpcUtil.getStatus(e);
+            log.info("[gRPC] cancelReservation error - {}", toJsonString(status));
+
+            responseObserver.onError(status
+                .withDescription(status.getDescription())
+                .asRuntimeException());
+        }
+    }
+}
