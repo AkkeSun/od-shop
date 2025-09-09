9e2368bfb3b9a1dd677a23951b71bbc1b6b417b3
od <akkessun@gmail.com>
2025-09-09 09:57:06 +0900

    feat(order): 주문 취소 API 개발 (in adapter 추가)

--- order/src/main/java/com/order/adapter/in/controller/cancel_order/CancelOrderController.java
+++ order/src/main/java/com/order/adapter/in/controller/cancel_order/CancelOrderController.java
@@ -1,18 +1,30 @@
 package com.order.adapter.in.controller.cancel_order;
 
+import com.order.applicatoin.port.in.CancelOrderUseCase;
+import com.order.applicatoin.port.in.command.CancelOrderCommand;
+import com.order.applicatoin.service.cancel_order.CancelOrderServiceResponse;
 import com.order.domain.model.Account;
 import com.order.infrastructure.resolver.LoginAccount;
 import com.order.infrastructure.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 class CancelOrderController {
 
+    private final CancelOrderUseCase useCase;
+
     @DeleteMapping("/orders/{orderId}")
-    ApiResponse<CancelOrderResponse> cancel(@LoginAccount Account account) {
-        return ApiResponse.ok(null);
+    ApiResponse<CancelOrderResponse> cancel(@LoginAccount Account account,
+        @PathVariable Long orderId) {
+        CancelOrderServiceResponse serviceResponse = useCase.cancel(CancelOrderCommand.builder()
+            .orderId(orderId)
+            .account(account)
+            .build());
+
+        return ApiResponse.ok(CancelOrderResponse.of(serviceResponse));
     }
 }
--- order/src/main/java/com/order/adapter/in/controller/cancel_order/CancelOrderResponse.java
+++ order/src/main/java/com/order/adapter/in/controller/cancel_order/CancelOrderResponse.java
@@ -1,7 +1,12 @@
 package com.order.adapter.in.controller.cancel_order;
 
+import com.order.applicatoin.service.cancel_order.CancelOrderServiceResponse;
+
 record CancelOrderResponse(
     Boolean result
 ) {
 
+    public static CancelOrderResponse of(CancelOrderServiceResponse serviceResponse) {
+        return new CancelOrderResponse(serviceResponse.result());
+    }
 }
