024950f6bec09e5aa3646dd27b40f5c395794741
od <akkessun@gmail.com>
2025-09-16 09:17:59 +0900

    feat(product): order 도메인 호출을 위한 proto service 구현

--- product/src/main/java/com/product/adapter/out/client/order/OrderClientAdapter.java
+++ product/src/main/java/com/product/adapter/out/client/order/OrderClientAdapter.java
@@ -1,24 +1,41 @@
 package com.product.adapter.out.client.order;
 
 import com.product.application.port.out.OrderClientPort;
+import grpc.product.ExistsCustomerProductRequest;
+import grpc.product.ExistsCustomerProductServiceGrpc.ExistsCustomerProductServiceBlockingStub;
+import grpc.product.FindOrderProductIdsRequest;
+import grpc.product.FindOrderProductIdsServiceGrpc.FindOrderProductIdsServiceBlockingStub;
 import java.util.List;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 
 @Component
 class OrderClientAdapter implements OrderClientPort {
 
-    // TODO
+    @GrpcClient("order")
+    private ExistsCustomerProductServiceBlockingStub existsCustomerProductService;
+
+    @GrpcClient("order")
+    private FindOrderProductIdsServiceBlockingStub findOrderProductIdsService;
+
     @Override
     public boolean isOrderValid(Long productId, Long accountId) {
-        return true;
+        return existsCustomerProductService.exists(
+                ExistsCustomerProductRequest.newBuilder()
+                    .setProductId(productId)
+                    .setCustomerId(accountId)
+                    .build())
+            .getExists();
     }
 
     @Override
     public List<Long> findProductIdByAccountId(Long accountId, int limit) {
-        return List.of(
-            1937295956458344448L,
-            1935511950012190720L
-        );
+        return findOrderProductIdsService.findIds(
+                FindOrderProductIdsRequest.newBuilder()
+                    .setCustomerId(accountId)
+                    .setLimit(limit)
+                    .build())
+            .getProductIdsList();
     }
 
 }
