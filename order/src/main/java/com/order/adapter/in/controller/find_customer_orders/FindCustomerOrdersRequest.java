b1471ab295089c83eb30cd99d77b362464553ed2
od <akkessun@gmail.com>
2025-10-09 11:33:58 +0900

    refactor(order): 요청 객체 생성자 롬복으로 변경

--- order/src/main/java/com/order/adapter/in/controller/find_customer_orders/FindCustomerOrdersRequest.java
+++ order/src/main/java/com/order/adapter/in/controller/find_customer_orders/FindCustomerOrdersRequest.java
@@ -2,13 +2,19 @@ package com.order.adapter.in.controller.find_customer_orders;
 
 import com.order.applicatoin.port.in.command.FindCustomerOrdersCommand;
 import com.order.domain.model.Account;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Builder
-record FindCustomerOrdersRequest(
-    Integer page,
-    Integer size
-) {
+@NoArgsConstructor
+@AllArgsConstructor
+class FindCustomerOrdersRequest {
+
+    private Integer page;
+    private Integer size;
 
     FindCustomerOrdersCommand toCommand(Account account) {
         return FindCustomerOrdersCommand.builder()
--- order/src/main/java/com/order/adapter/in/controller/find_sold_products/FindSoldProductsRequest.java
+++ order/src/main/java/com/order/adapter/in/controller/find_sold_products/FindSoldProductsRequest.java
@@ -3,12 +3,15 @@ package com.order.adapter.in.controller.find_sold_products;
 import com.order.applicatoin.port.in.command.FindSoldProductsCommand;
 import com.order.domain.model.Account;
 import com.order.infrastructure.validation.ValidSearchType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 class FindSoldProductsRequest {
 
     @ValidSearchType
@@ -16,14 +19,6 @@ class FindSoldProductsRequest {
     private Integer page;
     private Integer size;
     private String query;
-
-    @Builder
-    FindSoldProductsRequest(String searchType, Integer page, Integer size, String query) {
-        this.searchType = searchType;
-        this.page = page;
-        this.size = size;
-        this.query = query;
-    }
 
     FindSoldProductsCommand toCommand(Account account) {
         return FindSoldProductsCommand.builder()
