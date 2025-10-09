dd629ad78e6466fc3ea9fd9bbbdd2c32c207a28e
od <akkessun@gmail.com>
2025-10-09 22:36:39 +0900

    test

--- account-agent/src/main/java/com/accountagent/infrastructure/config/MongoConfig.java
+++ account-agent/src/main/java/com/accountagent/infrastructure/config/MongoConfig.java
@@ -3,6 +3,7 @@ package com.accountagent.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
@@ -11,11 +12,9 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 public class MongoConfig {
 
     @Bean
-    public MongoTemplate mongoTemplate(
-        MongoTemplate mongoTemplate,
-        MappingMongoConverter converter
-    ) {
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory,
+        MappingMongoConverter converter) {
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
-        return mongoTemplate;
+        return new MongoTemplate(mongoDbFactory, converter);
     }
 }
