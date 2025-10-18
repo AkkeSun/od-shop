b02518abbd8f006515a57f66752c83535af3e2f6
od <akkessun@gmail.com>
2025-10-18 09:00:57 +0900

    refactor(common): KafkaProducerConfig 공통 모듈화

--- account/src/main/java/com/account/AccountApplication.java
+++ account/src/main/java/com/account/AccountApplication.java
@@ -1,9 +1,12 @@
 package com.account;
 
+import com.common.infrastructure.config.KafkaProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KafkaProducerConfig.class)
 public class AccountApplication {
 
     public static void main(String[] args) {
--- product/src/main/java/com/product/ProductApplication.java
+++ product/src/main/java/com/product/ProductApplication.java
@@ -1,9 +1,12 @@
 package com.product;
 
+import com.common.infrastructure.config.KafkaProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KafkaProducerConfig.class)
 public class ProductApplication {
 
     public static void main(String[] args) {
