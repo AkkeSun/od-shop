1ddc55c9878f19abf04aa8124de0feeb3e86f1b5
od <akkessun@gmail.com>
2025-10-26 01:02:17 +0900

    test(product): IncreaseProductQuantityLockTest 추가

--- product/src/test/java/com/product/IntegrationTestSupport.java
+++ product/src/test/java/com/product/IntegrationTestSupport.java
@@ -3,12 +3,10 @@ package com.product;
 import com.common.infrastructure.util.SnowflakeGenerator;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.system.OutputCaptureExtension;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -24,10 +22,4 @@ public class IntegrationTestSupport {
 
     @MockBean(name = "shadedNettyGrpcServerLifecycle") // grpc mock 처리
     private Object grpcServerLifecycle;
-
-    @MockBean
-    protected RedissonClient redissonClient;
-
-    @MockBean
-    protected RedisTemplate<String, Object> redisTemplate;
 }
