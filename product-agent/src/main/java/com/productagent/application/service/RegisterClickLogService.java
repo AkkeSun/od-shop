573a57fc9f740728dc67eb1f1a5d5b845140d413
od <akkessun@gmail.com>
2025-11-04 09:47:24 +0900

    modify(product-agent): 불필요한 로직 제거

--- product-agent/src/main/java/com/productagent/application/service/RegisterClickLogService.java
+++ product-agent/src/main/java/com/productagent/application/service/RegisterClickLogService.java
@@ -4,13 +4,10 @@ import static com.common.infrastructure.util.JsonUtil.parseJson;
 
 import com.productagent.application.port.in.RegisterClickLogUseCase;
 import com.productagent.application.port.out.LogStoragePort;
-import com.productagent.domain.model.Product;
 import com.productagent.domain.model.ProductClickLog;
 import com.productagent.infrastructure.constant.CollectionName;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -28,7 +25,6 @@ class RegisterClickLogService implements RegisterClickLogUseCase {
     @Override
     public void register(ConsumerRecords<String, String> records) {
         List<ProductClickLog> logs = new ArrayList<>();
-        Map<Long, Product> productMap = new HashMap<>();
 
         for (ConsumerRecord<String, String> record : records) {
             String payload = record.value();
